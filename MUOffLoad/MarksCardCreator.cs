da0190fe73e80cdb6dc2019d94c7ef08158be452
Sree <sreehari.aranghat@outlook.com>
2016-04-23 12:36:08 +0530

    Added a called MarksCardCreator

---
+++ MUOffLoad/MarksCardCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MUOffLoad
+{
+    public enum ImportType
+    {
+        Regular
+        ,Consolidated
+    }
+
+    public class MarksCardCreator
+    {
+       static  MUPRJEntities entities = new MUPRJEntities();
+
+        public static void CreateMarksCard(string registernumber,ImportType type)
+        {
+             
+        }
+
+        public void ImportData()
+        {
+
+        }
+    }
+}
